5edadab45893bdc57677c3dc0a49d0d9b263ec5a
dbecerra-O <dbecerraorellana@gmail.com>
2025-04-03 15:22:50 -0500

    version2 StudentEntity

--- src/main/java/pe/edu/tecsup/lab03/entities/StudentEntity.java
+++ src/main/java/pe/edu/tecsup/lab03/entities/StudentEntity.java
@@ -3,6 +3,6 @@ package pe.edu.tecsup.lab03.entities;
 public class StudentEntity {
 
     public static void main(String[] args) {
-        System.out.println("Alumna: SANDRA SOFIA ANCHELIA DELGADO");
+        System.out.println("Hola soy SANDRA SOFIA ANCHELIA DELGADO, estoy haciendo el lab03");
     }
 }
