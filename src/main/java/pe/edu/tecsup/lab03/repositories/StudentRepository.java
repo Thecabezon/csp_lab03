283bad753fc691ba0dac048a444a56d45ce884f1
Sgiane11a <steyci.cordova@tecsup.edu.pe>
2025-04-03 15:18:09 -0500

    VERSION 2.0 Carpeta Repository (Gianella Cordova)

--- src/main/java/pe/edu/tecsup/lab03/repositories/StudentRepository.java
+++ src/main/java/pe/edu/tecsup/lab03/repositories/StudentRepository.java
@@ -2,6 +2,6 @@ package pe.edu.tecsup.lab03.repositories;
 
 public class StudentRepository {
     public static void main(String[] args) {
-        System.out.println("Nombre: GIANELLA CORDOVA APOLINARIO");
+        System.out.println("HOLA SOY GIANELLA CORDOVA ESTA CLASE ES MI PRACTICA");
     }
 }
