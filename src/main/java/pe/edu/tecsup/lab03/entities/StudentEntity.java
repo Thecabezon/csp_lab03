d895aea6307850ed992aec52dee341ee3b49dbc6
dbecerra-O <dbecerraorellana@gmail.com>
2025-04-03 14:51:06 -0500

    SUBIENDO StudentEntity

---
+++ src/main/java/pe/edu/tecsup/lab03/entities/StudentEntity.java
@@ -0,0 +1,8 @@
+package pe.edu.tecsup.lab03.entities;
+
+public class StudentEntity {
+
+    public static void main(String[] args) {
+        System.out.println("Alumna: SANDRA SOFIA ANCHELIA DELGADO");
+    }
+}
